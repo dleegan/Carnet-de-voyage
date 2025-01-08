5d7ca28c3573d68d96f1752253526b31f6cdd576
DUPROS Leegan <leegan.dupros@epitech.eu>
2025-01-09 00:48:24 +0100

    Initial Commit

---
+++ Carnet de voyage/Carnet_de_voyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carnet_de_voyageApp.swift
+//  Carnet de voyage
+//
+//  Created by dleegan on 09/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Carnet_de_voyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
